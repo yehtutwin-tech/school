eadef7748b2c377dd0c88e27ebfa8ec56b74e71d
Ye Htut Win <yehtutwin.tech@gmail.com>
2024-09-02 13:15:30 +0630

    course list display

--- courses/index.php
+++ courses/index.php
@@ -1,5 +1,15 @@
 <?php include_once('../partials/header.php'); ?>
 
+<?php
+  $sql = "SELECT * FROM `courses`";
+
+  $result = $conn->query($sql);
+
+  if (!$result) {
+    die('query failed: ' . $conn->error);
+  }
+?>
+
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Courses</h1>
@@ -15,21 +25,27 @@
       <thead>
         <tr>
           <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Description</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>
-            <a href="" class="btn btn-warning">
-              <span data-feather="edit"></span>
-            </a>
-            <a href="" class="btn btn-danger">
-              <span data-feather="trash"></span>
-            </a>
-          </td>
-        </tr>
+        <?php while($row = $result->fetch_assoc()) { ?>
+          <tr>
+            <td><?= $row['course_id'] ?></td>
+            <td><?= $row['name'] ?></td>
+            <td><?= $row['description'] ?></td>
+            <td>
+              <a href="" class="btn btn-warning">
+                <span data-feather="edit"></span>
+              </a>
+              <a href="" class="btn btn-danger">
+                <span data-feather="trash"></span>
+              </a>
+            </td>
+          </tr>
+        <?php } ?>
       </tbody>
     </table>
   </div>
